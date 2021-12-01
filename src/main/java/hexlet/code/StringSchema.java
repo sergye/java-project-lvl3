df31e5dae5558c556e1113f54479e5bf6ff40183
sergye <sergye@gmail.com>
2021-12-01 17:13:27 +0200

    Step 2

--- src/main/java/hexlet/code/StringSchema.java
+++ src/main/java/hexlet/code/StringSchema.java
@@ -7,6 +7,11 @@ public final class StringSchema extends Validator {
         return this;
     }
 
+    public StringSchema minLength(int minLength) {
+        this.addToValidationList(x -> x == null || (x instanceof String && ((String) x).length() >= minLength));
+        return this;
+    }
+
     public StringSchema contains(String str) {
         this.addToValidationList(x -> x == null || (x instanceof String && ((String) x).contains(str)));
         return this;
--- src/test/java/hexlet/code/ValidatorTest.java
+++ src/test/java/hexlet/code/ValidatorTest.java
@@ -6,12 +6,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ValidatorTest {
 
+    private static final int LENGTH = 5;
+
     @Test
     void testRequired() {
         Validator validator = new Validator();
         StringSchema schema = validator.string();
         assertThat(schema.required().isValid("")).isEqualTo(true);
         assertThat(schema.required().isValid(null)).isEqualTo(false);
+    }
+
+    @Test
+    void testMinLength() {
+        Validator validator = new Validator();
+        StringSchema schema = validator.string();
+        boolean expected = schema.minLength(LENGTH).isValid("12345");
+        assertThat(expected).isEqualTo(true);
+        expected = schema.minLength(LENGTH).isValid("1234");
+        assertThat(expected).isEqualTo(false);
     }
 
     @Test
